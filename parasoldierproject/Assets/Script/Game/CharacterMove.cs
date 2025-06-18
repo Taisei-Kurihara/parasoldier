5a548aad24bb1f7f8afcacbc83bffcbb6ba24f46
Taisei-Kurihara <163286893+Taisei-Kurihara@users.noreply.github.com>
2025-06-18 12:36:21 +0900

    攻撃等に対しての入力割り込みを調整中

--- parasoldierproject/Assets/Script/Game/CharacterMove.cs
+++ parasoldierproject/Assets/Script/Game/CharacterMove.cs
@@ -15,25 +15,32 @@ public class CharacterMove : MonoBehaviour
 
     #region 入力管理
 
+    #region 全体
+    BitArray generalOrder = new BitArray(2, true);
+
+    /// <summary> 戦闘前or決着後に動けなくする用 </summary>
+    public bool inputFailure { get { return generalOrder[0]; } }
+    private void unlockInput() { generalOrder[0] = false; }
+    private void disableInput() { generalOrder[0] = true; }
+
+    // / <summary> 入力割り込みできるか </summary>
+    public bool Interrupt { get { return generalOrder[1]; } set { generalOrder[1] = value; } }
+
+    #endregion
+
     #region 移動入力管理
 
-    BitArray moveInput = new BitArray(3, false);
+    BitArray moveInput = new BitArray(2, false);
 
     /// <summary> 現在移動入力中かどうか </summary>
     public bool NowMoveInput { get { return moveInput[0]; } }
     private bool SetNowMoveInput { set { moveInput[0] = value; } }
 
 
-    /// <summary> 戦闘前or決着後に動けなくする用 </summary>
-    public bool OperatingMovable { get { return moveInput[1]; } }
-    private void PermissionNotMovable() { moveInput[1] = false; }
-    private void PermissionMovable() { moveInput[1] = true; }
-
-
     /// <summary> 移動可能な状態か確認する </summary>
-    public bool CanItBeMoved { get { return moveInput[2]; } }
-    private void UnMove() { moveInput[2] = false; }
-    private void OnMove() { moveInput[2] = true; }
+    public bool CanItBeMoved { get { return moveInput[1]; } }
+    private void UnMove() { moveInput[1] = false; }
+    private void OnMove() { moveInput[1] = true; }
     #endregion
 
     #region 攻撃入力管理
@@ -59,7 +66,7 @@ public class CharacterMove : MonoBehaviour
     public void Init()
     {
         OnMove();
-
+        unlockInput();
         moveData.moveDis
             .Subscribe(_ => { if (!NowMoveInput) { MoveAsync().Forget(); } } )
             .AddTo(this);
@@ -107,15 +114,18 @@ public class CharacterMove : MonoBehaviour
 
     public void Attack()
     {
+        if (inputFailure || !Interrupt) return; // 入力が無効化されている場合は攻撃しない
         Attackasync().Forget();
     }
 
     async UniTask Attackasync()
     {
+        Interrupt = false; // 攻撃中は入力割り込みを無効化
         // 攻撃処理
         animator.SetTrigger("attack");
         await UniTask.Yield(token);
         await UniTask.Delay(1000, cancellationToken: token); // 攻撃アニメーションの再生時間に合わせて待機
+        Interrupt = true; // 攻撃が終わったら入力割り込みを有効化
     }
 
     #endregion
